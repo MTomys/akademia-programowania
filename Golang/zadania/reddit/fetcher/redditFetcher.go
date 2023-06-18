7c3df3fb243bf92bb41384cd14129461287d268a
MTomys <michalltomys@protonmail.ch>
2023-06-18 14:06:47 +0200

    add reddit solution

--- Golang/zadania/reddit/fetcher/redditFetcher.go
+++ Golang/zadania/reddit/fetcher/redditFetcher.go
@@ -1,7 +1,12 @@
 package fetcher
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
+	"log"
+	"net/http"
+	"time"
 )
 
 type response struct {
@@ -18,4 +23,58 @@ type response struct {
 type RedditFetcher interface {
 	Fetch() error
 	Save(io.Writer) error
+}
+
+type RedditClient struct {
+	responseData response
+	host         string
+	c            *http.Client
+}
+
+func NewRedditClient(host string, timeout time.Duration) *RedditClient {
+	return &RedditClient{
+		host:         host,
+		responseData: response{},
+		c: &http.Client{
+			Timeout: timeout,
+		},
+	}
+}
+
+func (rc *RedditClient) Fetch() error {
+	req, err := http.NewRequest(http.MethodGet, rc.host, nil)
+	if err != nil {
+		return fmt.Errorf("error occured while creating request %v", err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := rc.c.Do(req)
+	if err != nil {
+		return fmt.Errorf("error occured while getting data %v", err)
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&rc.responseData)
+	if err != nil {
+		return fmt.Errorf("error occured while decoding response body: %v", err)
+	}
+
+	log.Printf("successfully fetched data from %s\n", rc.host)
+
+	return nil
+}
+
+func (rc *RedditClient) Save(w io.Writer) error {
+	if len(rc.responseData.Data.Children) == 0 {
+		return fmt.Errorf("response data contained no children elements")
+	}
+
+	for _, v := range rc.responseData.Data.Children {
+		_, err := fmt.Fprintf(w, "Title: %s\nURL: %s\n", v.Data.Title, v.Data.URL)
+		if err != nil {
+			return fmt.Errorf("error occured while writing data: %v", err)
+		}
+	}
+
+	return nil
 }
--- Golang/zadania/reddit/main.go
+++ Golang/zadania/reddit/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"io"
+	"log"
+	"os"
 	"reddit/fetcher"
+	"time"
 )
 
 func main() {
 	var f fetcher.RedditFetcher // do not change
 	var w io.Writer             // do not change
 
-	f.Fetch()
-	f.Save(w)
+	f = fetcher.NewRedditClient("https://www.reddit.com/r/golang.json", 5*time.Second)
+	w, _ = os.Create("redditoutput.txt")
+
+	err := f.Fetch()
+	if err != nil {
+		log.Fatalf("error occured during fetching: %v", err)
+	}
+	err = f.Save(w)
+	if err != nil {
+		log.Fatalf("error occured during saving: %v", err)
+	}
 }
